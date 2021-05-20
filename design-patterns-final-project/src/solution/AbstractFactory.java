fc737b518ffd1dd605a7f132ed156217d49f69eb
Milcho <milchoarnaudov9807@abv.bg>
2021-05-20 15:49:47 +0300

    Delete FactoryProducer and move its responsibility to AbstractFactory

--- design-patterns-final-project/src/solution/AbstractFactory.java
+++ design-patterns-final-project/src/solution/AbstractFactory.java
@@ -3,5 +3,15 @@ package solution;
 import solution.Commands.Command;
 
 public abstract class AbstractFactory {
+	public static AbstractFactory getFactory(String factory) {
+		try {
+			String path = "solution.";
+			return (AbstractFactory) Class.forName(path + factory).getConstructor().newInstance();
+		} catch (Exception e) {
+			System.out.println("Factory not found");
+			return null;
+		}
+	}
+	
 	abstract Command getCommand(String command);
 }
--- design-patterns-final-project/src/solution/Main.java
+++ design-patterns-final-project/src/solution/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		FitnessInstructor fitnessInstructor = new FitnessInstructor();
 		
-		AbstractFactory commandFactory = FactoryProducer.getFactory(Constants.COMMAND_FACTORY);
+		AbstractFactory commandFactory = AbstractFactory.getFactory(Constants.COMMAND_FACTORY);
 		
 		Command layDown = commandFactory.getCommand(Constants.LAYDOWN_COMMAND);
 		Command standUp = commandFactory.getCommand(Constants.STANDUP_COMMAND);
